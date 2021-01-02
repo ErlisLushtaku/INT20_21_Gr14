e11903c604456d51937212056977980abd6703cf
rinajasharaj <75223541+rinajasharaj@users.noreply.github.com>
2021-01-02 23:06:59 +0100

    Create scroll.js

---
+++ js/scroll.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    var controller= new ScrollMagic.Controller();
+    var ourScene= new ScrollMagic.Scene({
+        triggerElement:'.hyrja-scroll',
+        reverse: false /* to prevent faded in contents disappear when page scrolled up*/
+    })
+
+    .setClassToggle('.hyrja-scroll', 'show')
+    .addTo(controller);
+})
+
