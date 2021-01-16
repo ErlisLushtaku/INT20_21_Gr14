c162fc3bd48752b0c9e325202987c3944d28eecc
Erlis Lushtaku <erlis.lushtaku@student.uni-pr.edu>
2021-01-16 17:07:19 +0100

    Added User database and password verification on login

--- login_validation.js
+++ login_validation.js
@@ -1,43 +1,69 @@
-var submit_form = new Array() ;
+var submit_form = new Array();
 
-function validation(){
-    username_validation();
-    password_validation();
-    for(var i=0;i < submit_form.length ;i++){
-      if (submit_form[i]==false)
-          return false;
+users = Array.from(JSON.parse(localStorage.getItem('users')));
+function validation() {
+  let username = document.getElementById("username").value;
+  var index = 0;
+  if (users != null) {
+    let exists = false;
+    for (let i = 0; i < users.length; i++) {
+      if (users[i].username == username) {
+        exists = true;
+        index = i;
+        break;
+      }
+    }
+    if (!exists) {
+      window.alert("You must Sign Up first!");
+      return false;
+    }
+  }
+  else {
+    return false;
+  }
+
+  username_validation();
+  password_validation(index);
+  for (var i = 0; i < submit_form.length; i++) {
+    if (submit_form[i] == false)
+      return false;
   }
   return true;
 }
 
-function username_validation(){
-  
-    var username_pattern = new RegExp(/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/);
-    var username = document.getElementById("username").value;
-  
-    if(username.match(username_pattern)){
-        document.getElementById("username").setAttribute("style","border:1px green solid ");
-        submit_form[0] = true;
-      }
-    else {
-        window.alert("Incorrect username")
-        document.getElementById("username").setAttribute("style","border:1px red solid ");
-        submit_form[0] = false;
-      }
-}
-function password_validation(){
-  
-    var password_pattern = new RegExp(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/);
-    var password = document.getElementById("password").value;
-   
-    if(password.match(password_pattern)){
-        document.getElementById("password").setAttribute("style","border:1px green solid ");
-        submit_form[1] = true;
-      }
-    else {
-        window.alert("Incorrect password")
-        document.getElementById("password").setAttribute("style","border:1px red solid ");
-        submit_form[1] = false;
-      }
-}
+function username_validation() {
 
+  var username_pattern = new RegExp(/^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/);
+  var username = document.getElementById("username").value;
+  
+  if (username.match(username_pattern)) {
+    document.getElementById("username").setAttribute("style", "border:1px green solid ");
+    submit_form[0] = true;
+  }
+  else {
+    window.alert("Incorrect username")
+    document.getElementById("username").setAttribute("style", "border:1px red solid ");
+    submit_form[0] = false;
+  }
+}
+function password_validation(index) {
+
+  var password_pattern = new RegExp(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/);
+  var password = document.getElementById("password").value;
+
+  if(users[index].password != password) {
+    window.alert("Incorrect password")
+    document.getElementById("password").setAttribute("style", "border:1px red solid ");
+    submit_form[1] = false;
+  }
+  
+  if (password.match(password_pattern)) {
+    document.getElementById("password").setAttribute("style", "border:1px green solid ");
+    submit_form[1] = true;
+  }
+  else {
+    window.alert("Incorrect password")
+    document.getElementById("password").setAttribute("style", "border:1px red solid ");
+    submit_form[1] = false;
+  }
+}
--- signup_validation.js
+++ signup_validation.js
@@ -1,55 +1,79 @@
-var submit_form = new Array() ;
-
-function signup_validation(){
-    username1_validation();
-    password1_validation();
-    email1_validation();
-    for(var i=0;i < submit_form.length ;i++){
-      if (submit_form[i]==false)
-          return false;
+var submit_form = new Array();
+var users = Array.from(JSON.parse(localStorage.getItem('users')));
+function signup_validation() {  
+  username1_validation();
+  password1_validation();
+  email1_validation();
+  for (var i = 0; i < submit_form.length; i++) {
+    if (submit_form[i] == false)
+      return false;
   }
+  addUser();
   return true;
 }
 
-function username1_validation(){
-  
-    var username_pattern = new RegExp(/^(?=.{8,20}$)(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/);
-    var username = document.getElementById("username1").value;
-  
-    if(username.match(username_pattern)){
-        $("#username1").css("border", "1px solid green")
-        submit_form[0] = true;
-      }
-    else {
-        window.alert("Username must be 8-20 charachters-long,no space in between charachters, no _ or . at the end ")
-        document.getElementById("username1").setAttribute("style","border:1px red solid ");
-        submit_form[0] = false;
-      }
+function username1_validation() {
+
+  var username_pattern = new RegExp(/^(?=.{8,20}$)(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$/);
+  var username = document.getElementById("username1").value;
+
+  if (username.match(username_pattern)) {
+    $("#username1").css("border", "1px solid green")
+    submit_form[0] = true;
+  }
+  else {
+    window.alert("Username must be 8-20 charachters-long,no space in between charachters, no _ or . at the end ")
+    document.getElementById("username1").setAttribute("style", "border:1px red solid ");
+    submit_form[0] = false;
+  }
 }
-function password1_validation(){
-  
-    var password_pattern = new RegExp(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/);
-    var password = document.getElementById("password1").value;
-   
-    if(password.match(password_pattern)){
-        document.getElementById("password1").setAttribute("style","border:1px green solid ");
-        submit_form[1] = true;
-      }
-    else {
-        window.alert("Password must be At least 8 characters long one lowercase, one uppercase, one number and one special character; ");
-        document.getElementById("password1").setAttribute("style","border:1px red solid ");
-        submit_form[1] = false;
-      }
+function password1_validation() {
+
+  var password_pattern = new RegExp(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/);
+  var password = document.getElementById("password1").value;
+
+  if (password.match(password_pattern)) {
+    document.getElementById("password1").setAttribute("style", "border:1px green solid ");
+    submit_form[1] = true;
+  }
+  else {
+    window.alert("Password must be At least 8 characters long one lowercase, one uppercase, one number and one special character; ");
+    document.getElementById("password1").setAttribute("style", "border:1px red solid ");
+    submit_form[1] = false;
+  }
 }
 
-function email1_validation(){
-    const email_pattern = new RegExp(/^[^ ]+@[^ ]+\.[a-z]{2,3}$/);
-    const email = document.getElementById("email1").value;
-    if(email.match(email_pattern)){
-        document.getElementById("email1").setAttribute("style","border:1px green solid ");
-        submit_form[0] = true;}
-    else{
-        window.alert("Email format: someone@example.com")
-        document.getElementById("email1").setAttribute("style","border:1px red solid ");
-        submit_form[0] = false;}
+function email1_validation() {
+  const email_pattern = new RegExp(/^[^ ]+@[^ ]+\.[a-z]{2,3}$/);
+  const email = document.getElementById("email1").value;
+  if (email.match(email_pattern)) {
+    document.getElementById("email1").setAttribute("style", "border:1px green solid ");
+    submit_form[0] = true;
   }
+  else {
+    window.alert("Email format: someone@example.com")
+    document.getElementById("email1").setAttribute("style", "border:1px red solid ");
+    submit_form[0] = false;
+  }
+}
+
+function User(id, username, email, password) {
+  this.id = id;
+  this.username = username;
+  this.email = email;
+  this.password = password;
+}
+
+function addUser() {
+  let username = document.getElementById("username1").value;
+  let email = document.getElementById("email1").value;
+  let password = document.getElementById("password1").value;
+
+  let user = new User(users.length, username, email, password);
+  users.push(user);
+}
+
+window.onbeforeunload = function () {
+  console.log("unloaded");
+  localStorage.setItem('users', JSON.stringify(users));
+};
